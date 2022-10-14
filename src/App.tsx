fd6105e90b8d68a9f6bba4f2bbb976fe55278ef2
Bruno Accorsi Bergoli <b.bergoli@gmail.com>
2022-10-13 21:31:55 -0300

    Tests and mock

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import { Heading } from './components/Heading'
-import { Text } from './components/Text'
+import { SignIn } from './pages/SignIn'
 import './styles/global.css'
 
 export function App() {
   return (
-    <div className='mx-auto flex flex-col items-center'>
-      <div className='flex flex-col gap-1 items-center mt-56'>
-         <img src='src/source/react.svg' className='mb-7'/>
-         <Heading size='lg'>Ignite Lab</Heading>
-         <Text size='sm'>Login and start using</Text>
-      </div>
-      
-      <div>
-        
-      </div>
-    </div>
+    <SignIn />
   )
 }
 
